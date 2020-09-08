f2bf2b9585365ec241d553d3fb90762540ee6866
NIKHIL SINGH <nikhil.4598@gmail.com>
2020-09-08 11:20:42 +0530

    initial-commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const quizQuestion = [
+    {
+        question : 'What is the most used Programming Language in 2019 ?',
+        a:'Java',
+        b:'python',
+        c:'Ruby',
+        d:'JavaScript',
+        correct:'d'
+    },{
+        question : 'Who owns Instagram ?',
+        a:'Microsoft',
+        b:'Instagram',
+        c:'SnapChat',
+        d:'Facebook',
+        correct:'d'  
+    },{
+        question : 'Galileo was an astronomer who',
+        a:'developed the telescope',
+        b:'discovered four satellites of Jupiter',
+        c:'discovered that the movement of pendulum produces a regular time measurement ',
+        d:'All the above.',
+        correct:'b'  
+    },{
+        question : 'The currency notes are printed in',
+        a:'New Delhi ',
+        b:'Nagpur',
+        c:'Nasik',
+        d:'Bombay',
+        correct:'c'  
+    },{
+        question : 'The Indian, who holds the pride of beating the computers in mathematical wizard is:',
+        a:'Shakuntala Devi',
+        b:'Raja Ramanna',
+        c:'Ramanujam',
+        d:'Rina PanigrahiBombay',
+        correct:'a'  
+    },{
+        question : 'Name the best-selling book series of the 21st century? ',
+        a:'A Song Of Ice And Fire',
+        b:'Harry Potter',
+        c:'Lord Of The Rings',
+        d:'Robert Langdon Series',
+        correct:'b'  
+    }
+]
